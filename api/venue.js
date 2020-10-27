9c927fcd44257f94d97788b6e0e06bd3160d37db
James Taylor <jt-git@nti.me.uk>
2020-10-27 09:45:29 +0000

    Add lat/lon values to venue API
    Signed-off-by: James Taylor <jt-git@nti.me.uk>

--- api/venue.js
+++ api/venue.js
@@ -8,44 +8,68 @@ const nhsPublicKey = '-----BEGIN PUBLIC KEY-----\n' +
     'MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEVyPX5CqxSdg0JE/2O0wog0qmvza/\nGLVAxNmJROQwMjtzk1U5YESBvk7njod0LJwVTmw+w/aIRvvnQsB1w5CW1g==\n' +
     '-----END PUBLIC KEY-----\n';
 
-// curl -w "%{http_code}" -H "Content-Type: application/json" -X POST "localhost:3000/api/venue" -d '{"qr":"UKC19TRACING:1:eyJhbGciOiJFUzI1NiIsImtpZCI6IllycWVMVHE4ei1vZkg1bnpsYVNHbllSZkI5YnU5eVBsV1lVXzJiNnFYT1EifQ.eyJpZCI6IjVZM1Y0Mk01Iiwib3BuIjoiQmFybmFyZCBDYXN0bGUiLCJ2dCI6IjAwMCIsInBjIjoiREwxMjhQUiJ9.gINSi7vSZ3hnYceo1BvKF2TLeQHcCpiYbBxn6MvljICKGEfamrT6E1anNpyJl7kAhV0QYO6pQEFsJvPUhE91Iw"}'
-// curl -w "%{http_code}" -H "Content-Type: application/json" -X POST "https://barnard-castle.jtonline.vercel.app/api/venue" -d '{"qr":"UKC19TRACING:1:eyJhbGciOiJFUzI1NiIsImtpZCI6IllycWVMVHE4ei1vZkg1bnpsYVNHbllSZkI5YnU5eVBsV1lVXzJiNnFYT1EifQ.eyJpZCI6IjVZM1Y0Mk01Iiwib3BuIjoiQmFybmFyZCBDYXN0bGUiLCJ2dCI6IjAwMCIsInBjIjoiREwxMjhQUiJ9.gINSi7vSZ3hnYceo1BvKF2TLeQHcCpiYbBxn6MvljICKGEfamrT6E1anNpyJl7kAhV0QYO6pQEFsJvPUhE91Iw"}'
+// Bounding box to validate position of venues
+// -14.02,49.67,2.09,61.06 is too big but will do for now!
+const minLat=49.67;
+const maxLat=61.06;
+const minLon=-14.02;
+const maxLon=2.09;
 
-// const testToken = 'eyJhbGciOiJFUzI1NiIsImtpZCI6IllycWVMVHE4ei1vZkg1bnpsYVNHbllSZkI5YnU5eVBsV1lVXzJiNnFYT1EifQ.eyJpZCI6IjVZM1Y0Mk01Iiwib3BuIjoiQmFybmFyZCBDYXN0bGUiLCJ2dCI6IjAwMCIsInBjIjoiREwxMjhQUiJ9.gINSi7vSZ3hnYceo1BvKF2TLeQHcCpiYbBxn6MvljICKGEfamrT6E1anNpyJl7kAhV0QYO6pQEFsJvPUhE91Iw';
-// const fakeToken = 'bananas';
-// const fakeKey = '-----BEGIN PUBLIC KEY-----\n' +
-//     'MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgHAbM/BAHdbUY6ORe3/YAQwwd+Cb\n' +
-//     'HyFgYF7qOSJECYOqYzQ1JMoP1WDuLP4z/wBn/T73BeCz5kfY13AHBbSHXI2j3EIU\n' +
-//     'AHAX+SBYkHS/0hNdPM64n3t75D3wqWdXfNDyNF3ZB1rH2YpFbzaSkNVXEwG97U5R\n' +
-//     'JVgzXNaODqNxnbXjAgMBAAE=\n' +
-//     '-----END PUBLIC KEY-----';
+function getErrorResponse(reason) {
+    return {
+        reason: reason
+    }
+}
 
 module.exports = (req, res) => {
-    var code;
-    if (req.method == 'POST' && req.body && req.body.qr) {
-        code = req.body.qr;
+    if (req.method !== 'POST') {
+        return res.status(400).json(getErrorResponse('InvalidMethod'));
     }
 
+    if (!(req.body && req.body.qr && req.body.lat && req.body.lon)) {
+        return res.status(400).json(getErrorResponse('BadRequest'));
+    }
+    
+    const code = req.body.qr;
+    const lat = req.body.lat;
+    const lon = req.body.lon;
+
+    // Validate the lat and lon values
+    if (isNaN(lat) || lat < minLat || lat > maxLat) {
+        return res.status(400).json(getErrorResponse('InvalidLat'));
+    }
+
+    if (isNaN(lon) || lon < minLon || lon > maxLon) {
+        return res.status(400).json(getErrorResponse('InvalidLon'));
+    }
+
+    // Validate the QR data
     var token;
     if (code && code.startsWith('UKC19TRACING:1:')) {
         token = code.replace('UKC19TRACING:1:', '');
+    } else {
+        return res.status(400).json(getErrorResponse('InvalidQr'));
     }
 
     var venue;
     if (token) {
         jwt.verify(token, nhsPublicKey, function (err, decoded) {
-            if (decoded && !err) {
+            if (!err) {
                 venue = decoded;
             }
         });
     }
 
-    if (!venue) {
-        return res.status(400).send('');
+    if (!(venue && venue.id)) {
+        return res.status(400).json(getErrorResponse('InvalidVenue'));
     }
 
+    // Publish the venue
     var message = {
-        id: venue.id
+        id: venue.id,
+        pc: venue.pc,
+        lat: req.body.lat,
+        lon: req.body.lon
     }
 
     let jsonMessage = JSON.stringify(message, null, 2);
@@ -55,7 +79,7 @@ module.exports = (req, res) => {
     mqttClient.on('connect', function () {
         mqttClient.publish(mqttTopic, jsonMessage, function () {
             mqttClient.end();
-            res.status(202).json(message);
+            return res.status(202).json(message);
         });
     });
 }
